4ef6e3c8505b12e2a237e1cd732698c815a5db4b
Michael <neveragain1995@163.com>
2016-05-12 22:33:56 +0800

    Big update!

---
+++ scripts/blog/custom.js
@@ -0,0 +1,94 @@
+// Jquery with no conflict
+jQuery(document).ready(function ($) {
+
+
+    //##########################################
+    // Top Widget
+    //##########################################
+
+    var topContainer = $("#top-widget");
+    var topTrigger = $("#top-open");
+
+    topTrigger.click(function () {
+        topContainer.animate({
+            height: 'toggle'
+        });
+
+        if (topTrigger.hasClass('tab-closed')) {
+            topTrigger.removeClass('tab-closed');
+        } else {
+            topTrigger.addClass('tab-closed');
+        }
+
+        return false;
+
+    });
+
+    //##########################################
+    // Masonry
+    //##########################################
+
+
+    var $container = $('.portfolio-list');
+
+    $container.imagesLoaded(function () {
+        $container.masonry({
+            itemSelector: 'figure',
+            isAnimated: true
+        });
+    });
+
+
+    //##########################################
+    // Accordion box
+    //##########################################
+
+    $('.accordion-container').hide();
+    $('.accordion-trigger:first').addClass('active').next().show();
+    $('.accordion-trigger').click(function () {
+        if ($(this).next().is(':hidden')) {
+            $('.accordion-trigger').removeClass('active').next().slideUp();
+            $(this).toggleClass('active').next().slideDown();
+        }
+        return false;
+    });
+
+    //##########################################
+    // Toggle box
+    //##########################################
+
+    $('.toggle-trigger').click(function () {
+        $(this).next().toggle('slow');
+        $(this).toggleClass("active");
+        return false;
+    }).next().hide();
+
+    //##########################################
+    // Logo to popmenu
+    //##########################################
+
+    $(function () {
+        $('#logo').popmenu();
+    });
+    
+//close			
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- scripts/commonscript.js
+++ scripts/commonscript.js
@@ -6,4 +6,3 @@ $(".nav li").click(function () {
     $(this).addClass("active")
         .siblings().removeClass("active");
 });
-
